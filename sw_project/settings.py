daa1f23ec8869f754319834a6ba5e54b3c1297ad
Micha Hulsbosch <micha.hulsbosch@ru.nl>
2023-09-19 12:28:01 +0200

    feat: Add Postgres to Docker setup

--- sw_project/settings.py
+++ sw_project/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'sw_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": config("SQL_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": config("SQL_DATABASE", default=BASE_DIR / "db.sqlite3"),
+        "USER": config("SQL_USER", default=""),
+        "PASSWORD": config("SQL_PASSWORD", default=""),
+        "HOST": config("SQL_HOST", default=""),
+        "PORT": config("SQL_PORT", default=""),
     }
 }
 
