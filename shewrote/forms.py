615dcf280376df811c707cb51341b5cb0c16cfda
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-01-11 10:29:33 +0100

    feat: Add AddAnother for Person

--- shewrote/forms.py
+++ shewrote/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 from django_select2.forms import ModelSelect2Widget, ModelSelect2MultipleWidget
+from django.utils.safestring import SafeString
 
 from .models import Person, Place
+
+
+class AddAnotherWidget(ModelSelect2Widget):
+    def render(self, name, value, attrs=None, renderer=None):
+        output = super(AddAnotherWidget, self).render(name, value, attrs=attrs, renderer=renderer)
+        print(output)
+        output += """<a href="#" data-bs-toggle="modal" data-bs-target="#addanotherModal">
+                    <span data-toggle="tooltip" data-original-title="Add another" title="New">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
+                    </span>
+                    </a>"""
+        return SafeString(output)
 
 
 class PersonForm(forms.ModelForm):
@@ -44,10 +60,33 @@ class PersonForm(forms.ModelForm):
                                                      attrs={'data-placeholder': "Select a place"}),
             'place_of_death': ModelSelect2Widget(model=Place, search_fields=['name__icontains'],
                                                      attrs={'data-placeholder': "Select a place"}),
-            'mother': ModelSelect2Widget(model=Person, search_fields=['short_name__icontains'],
-                                                     attrs={'data-placeholder': "Select a person"}),
-            'father': ModelSelect2Widget(model=Person, search_fields=['short_name__icontains'],
-                                                     attrs={'data-placeholder': "Select a person"}),
+            'mother': AddAnotherWidget(model=Person, search_fields=['short_name__icontains'],
+                                       attrs={'data-placeholder': "Select a person"}),
+            'father': AddAnotherWidget(model=Person, search_fields=['short_name__icontains'],
+                                       attrs={'data-placeholder': "Select a person"}),
             'related_to': ModelSelect2MultipleWidget(model=Person, search_fields=['short_name__icontains'],
                                                      attrs={'data-placeholder': "Select multiple persons"})
+        }
+
+
+class ShortPersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = [
+            'short_name', 'viaf_or_cerl', 'first_name', 'maiden_name', 'date_of_birth', 'date_of_death',
+            'sex', 'notes',
+        ]
+        labels = {
+            'short_name': 'Short name',
+            'viaf_or_cerl': 'VIAF or CERL',
+            'first_name': 'First name',
+            'maiden_name': 'Maiden name',
+            'date_of_birth': 'Date of birth',
+            'date_of_death': 'Date of death',
+            'sex': 'Gender',
+            'notes': 'Notes',
+        }
+
+        widgets = {
+            'notes': forms.Textarea(attrs={'cols': 80}),
         }
--- shewrote/urls.py
+++ shewrote/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('persons/<uuid:person_id>/', views.person, name='person'),
     # Page for adding a new person
     path('new_person/', views.new_person, name='new_person'),
+    # Page for adding a new person (short version)
+    path('short_new_person/', views.short_new_person, name='short_new_person'),
     # Page for editing an existing person
     path('edit_person/<uuid:person_id>/', views.edit_person, name='edit_person'),
 ]
