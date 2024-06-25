2b4a915efc326d42fbf7f2fdb1fb9422b1b905c8
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-06-25 10:15:42 +0200

    fix: Also find year range in original_data.date

--- shewrote/management/commands/populate_work_date.py
+++ shewrote/management/commands/populate_work_date.py
@@ -12,24 +12,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.number_of_works_done = 0
-        for work in Work.objects.all():
+        for work in Work.objects.filter(original_data__isnull=False):
             date_str = work.original_data.get('date', '')
-            # XX-XX-XXXX
-            m = re.search(r'\d\d\-\d\d\-(\d\d\d\d)', date_str)
-            if m and m[1]:
-                self.save_work(work, date_str, m[1])
-                continue
 
-            m = re.search(r'\d\d\d\d', date_str)
-            if m and m[0]:
-                self.save_work(work, date_str, m[0])
-                continue
+            # Two years
+            if (m := re.search(r'(?<!\d)(\d{4})\D+(\d{4})(?!\d)', date_str)) and m[1] and m[2]:
+                self.save_work(work, date_str, m[1], m[2])
 
-            self.save_work(work, date_str, None)
+            # One year
+            elif (m := re.search(r'(?<!\d)(\d{3,4})(?!\d)', date_str)) and m[1]:
+                self.save_work(work, date_str, m[1], m[1])
 
-    def save_work(self, work, date_str, year):
-        work.date_of_publication_start = year
-        work.date_of_publication_end = year
+            # No year
+            else:
+                self.save_work(work, date_str, None, None)
+
+    def save_work(self, work, date_str, year_start, year_end):
+        work.date_of_publication_start = year_start
+        work.date_of_publication_end = year_end
         work.date_of_publication_text = date_str
         work.save()
 
