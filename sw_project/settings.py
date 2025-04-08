86c2308bf0ecffc450eec0571c15a498a90bba54
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-04-08 12:34:14 +0200

    feat: Add 404 and 500 pages #104

--- sw_project/settings.py
+++ sw_project/settings.py
@@ -96,7 +96,7 @@ ROOT_URLCONF = 'sw_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
