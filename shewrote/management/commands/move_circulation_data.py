e17b7b8487ab3445b3f29b087089bd83b42f832a
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-05-14 12:23:09 +0200

    feat: Add option to match strings to first N words of Reception title #110

--- shewrote/management/commands/move_circulation_data.py
+++ shewrote/management/commands/move_circulation_data.py
@@ -12,9 +12,31 @@ class Command(BaseCommand):
     strings_to_match = ['catalogue', 'presence']
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            "--nwords",
+            action="store",
+            help="The number of words at the beginning of the reception title that should contain the strings to match.",
+        )
+
+    def handle_arguments(self, **options):
+        n_words = options.get('nwords', None)
+        if n_words and not n_words.isdigit():
+            print('Argument --n-words should be a number.')
+            exit(1)
+        self.n_words = int(n_words) if n_words else None
+
+    def match_string_to_first_n_words(self, string):
+        words = [word.lower() for word in string.split()[:self.n_words]]
+        strings_to_match = [string.lower() for string in self.strings_to_match]
+        for string_to_match in strings_to_match:
+            for word in words:
+                if string_to_match in word:
+                    return True
+        return False
 
     def handle(self, *args, **options):
+        self.handle_arguments(**options)
+
         reception_filter = reduce(operator.or_, (Q(title__icontains=string) for string in self.strings_to_match))
         receptions_to_move = (Reception.objects.filter(reception_filter)
                               .prefetch_related('personreception_set', 'workreception_set', 'editionreception_set'))
@@ -23,7 +45,11 @@ class Command(BaseCommand):
         new_personcirculations = []
         new_workcirculations = []
         new_editionscirculations = []
+        reception_ids_to_delete = []
         for reception in receptions_to_move:
+            if self.n_words and not self.match_string_to_first_n_words(reception.title):
+                print(f'Unmatched reception: {reception.title} ({reception.id})')
+                continue
             circulation = Circulation(**{field_name: getattr(reception, field_name) for field_name in fields_to_move})
             new_circulations.append(circulation)
             new_personcirculations.extend([
@@ -38,6 +64,7 @@ class Command(BaseCommand):
                 EditionCirculation(circulation=circulation, edition=edition_reception.edition, type=edition_reception.type)
                 for edition_reception in reception.editionreception_set.all()
             ])
+            reception_ids_to_delete.append(reception.id)
 
         print('Creating Circulations and related objects.')
         Circulation.objects.bulk_create(new_circulations)
@@ -46,7 +73,7 @@ class Command(BaseCommand):
         EditionCirculation.objects.bulk_create(new_editionscirculations)
 
         print('Deleting the following:')
-        print(PersonReception.objects.filter(reception__in=receptions_to_move).delete())
-        print(WorkReception.objects.filter(reception__in=receptions_to_move).delete())
-        print(EditionReception.objects.filter(reception__in=receptions_to_move).delete())
-        print(receptions_to_move.delete())
+        print(PersonReception.objects.filter(reception_id__in=reception_ids_to_delete).delete())
+        print(WorkReception.objects.filter(reception_id__in=reception_ids_to_delete).delete())
+        print(EditionReception.objects.filter(reception_id__in=reception_ids_to_delete).delete())
+        print(Reception.objects.filter(id__in=reception_ids_to_delete).delete())
