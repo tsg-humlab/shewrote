70130d06200291a4227862c2e5bc8f9fb43e7121
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-05-19 14:50:45 +0200

    feat: Add part_of_work Receptions to Works on Person details page

--- shewrote/models.py
+++ shewrote/models.py
@@ -670,7 +670,7 @@ class Reception(EasyAuditMixin, models.Model):
     title = models.TextField(blank=True)
     is_same_as_work = models.ForeignKey(Work, models.SET_NULL, null=True, blank=True, related_name="is_same_as_reception",
                                         verbose_name="is same as work")
-    part_of_work = models.ForeignKey(Work, models.SET_NULL, null=True, blank=True, related_name="+")
+    part_of_work = models.ForeignKey(Work, models.SET_NULL, null=True, blank=True, related_name="contained_receptions")
     reference = models.TextField(blank=True)
     place_of_reception = models.ForeignKey(Place, models.PROTECT, null=True, blank=True)
     date_of_reception = models.IntegerField(blank=True, null=True)
