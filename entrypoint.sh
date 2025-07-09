5a9336ad59805bc88d9bbcf34abd394d356b4458
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-07-09 17:01:24 +0200

    refactor: Move dependency health check from entrypoinst.sh to docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,5 @@
 #!/bin/sh
 
-# Check that Postgres is running
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for Postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
-# Check that Redis is running
-if ! [ -z $REDIS_HOST ]
-then
-  echo "Waiting for Redis..."
-
-  while ! nc -z $REDIS_HOST $REDIS_PORT; do
-    sleep 0.1
-  done
-
-  echo "Redis started"
-fi
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
